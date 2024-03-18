7920130c5180070083defc7dcee6d2a26cced197
David Haworth <dh@thelancashireman.org>
2024-03-18 17:47:04 +0100

    Remove RP2040_ prefixes from macros

--- c/rp2040-clocks.c
+++ c/rp2040-clocks.c
@@ -24,11 +24,11 @@
 
 /* ToDo: allow these to be configured externally
 */
-#define APP_REFDIV		1		/* VCO input at 12 MHz */
-#define APP_FBDIV		133		/* VCO at 1596 MHz */
-#define APP_POSTDIV1	6		/* Div1 output at 266 MHz */
-#define APP_POSTDIV2	2		/* Div2 output at 133 MHz */
-#define APP_POSTDIVS	((APP_POSTDIV1 << 16) | (APP_POSTDIV2 << 12))
+#define APP_REFDIV			1		/* VCO input at 12 MHz */
+#define APP_FBDIV			133		/* VCO at 1596 MHz */
+#define APP_POSTDIV1		6		/* Div1 output at 266 MHz */
+#define APP_POSTDIV2		2		/* Div2 output at 133 MHz */
+#define APP_POSTDIVS		((APP_POSTDIV1 << 16) | (APP_POSTDIV2 << 12))
 
 #define APP_USB_REFDIV		APP_REFDIV			
 #define APP_USB_FBDIV		120		/* VCO at 1440 MHz */
@@ -41,15 +41,15 @@ void rp2040_clock_init(void)
 {
 	rp2040_clocks.sys_resus_ctrl = 0x00;				/* Disable resuscitation for now */
 
-	rp2040_xosc.ctrl = RP2040_XOSC_1_15_MHZ;			/* Should be fixed according to datasheet, but is R/W */
+	rp2040_xosc.ctrl = XOSC_1_15_MHZ;			/* Should be fixed according to datasheet, but is R/W */
 	rp2040_xosc.startup = ((12000000/1000)+255)/256;	/* 1 ms at 12 MHz, rounded up */
-	rp2040_xosc_w1s.ctrl = RP2040_XOSC_ENABLE;
-	do {	/* Wait */	} while ( (rp2040_xosc.status & RP2040_XOSC_STABLE) == 0 );
+	rp2040_xosc_w1s.ctrl = XOSC_ENABLE;
+	do {	/* Wait */	} while ( (rp2040_xosc.status & XOSC_STABLE) == 0 );
 
-	rp2040_clocks.ref.ctrl = RP2040_CLKSRC_REF_XOSC;	/* Select XOSC as the reference clock source */
-	rp2040_clocks.sys.ctrl = RP2040_CLKSRC_SYS_REF;		/* Select REF as the system clock source */
+	rp2040_clocks.ref.ctrl = CLKSRC_REF_XOSC;	/* Select XOSC as the reference clock source */
+	rp2040_clocks.sys.ctrl = CLKSRC_SYS_REF;		/* Select REF as the system clock source */
 
-	rp2040_clocks.peri.ctrl = RP2040_CLK_ENABLE | RP2040_CLKSRC_PERI_XOSC;		/* Select xosc as peripheral clock */
+	rp2040_clocks.peri.ctrl = CLK_ENABLE | CLKSRC_PERI_XOSC;		/* Select xosc as peripheral clock */
 }
 
 static void pll_helper(rp2040_pll_t *pll, rp2040_pll_t *pll_w1c, u32_t fbdiv, u32_t postdivs);
@@ -58,15 +58,15 @@ void rp2040_pll_init(void)
 {
 	/* Check if PLL is already correctly configured and running
 	*/
-	if ( (rp2040_pll.cs & RP2040_PLL_LOCK) != 0 &&
-		 (rp2040_pll.cs & RP2040_PLL_REFDIV) == APP_REFDIV &&
-		 (rp2040_pll.fbdiv_int & RP2040_PLL_FBDIV) == APP_FBDIV &&
-		 (rp2040_pll.prim & (RP2040_PLL_POSTDIV1 | RP2040_PLL_POSTDIV2)) == APP_POSTDIVS )
+	if ( (rp2040_pll.cs & PLL_LOCK) != 0 &&
+		 (rp2040_pll.cs & PLL_REFDIV) == APP_REFDIV &&
+		 (rp2040_pll.fbdiv_int & PLL_FBDIV) == APP_FBDIV &&
+		 (rp2040_pll.prim & (PLL_POSTDIV1 | PLL_POSTDIV2)) == APP_POSTDIVS )
 		return;
 
 	/* Reset and re-enable the PLL.
 	*/
-	rp2040_reset(RP2040_RESETS_pll_sys);
+	rp2040_reset(RESETS_pll_sys);
 
 	/* Initialise the main PLL
 	*/
@@ -74,23 +74,23 @@ void rp2040_pll_init(void)
 
 	/* Switch the sys clock to the PLL
 	*/
-	rp2040_clocks.sys.ctrl = RP2040_CLKSRC_SYS_REF | RP2040_CLKSRC_SYS_AUX_PLL;	/* Should be this already */
-	rp2040_clocks.sys.ctrl = RP2040_CLKSRC_SYS_AUX | RP2040_CLKSRC_SYS_AUX_PLL;	/* Switch to the aux clock */
+	rp2040_clocks.sys.ctrl = CLKSRC_SYS_REF | CLKSRC_SYS_AUX_PLL;	/* Should be this already */
+	rp2040_clocks.sys.ctrl = CLKSRC_SYS_AUX | CLKSRC_SYS_AUX_PLL;	/* Switch to the aux clock */
 }
 
 void rp2040_usbpll_init(void)
 {
 	/* Check if USB PLL is already correctly configured and running
 	*/
-	if ( (rp2040_usbpll.cs & RP2040_PLL_LOCK) != 0 &&
-		 (rp2040_usbpll.cs & RP2040_PLL_REFDIV) == APP_REFDIV &&
-		 (rp2040_usbpll.fbdiv_int & RP2040_PLL_FBDIV) == APP_USB_FBDIV &&
-		 (rp2040_usbpll.prim & (RP2040_PLL_POSTDIV1 | RP2040_PLL_POSTDIV2)) == APP_USB_POSTDIVS )
+	if ( (rp2040_usbpll.cs & PLL_LOCK) != 0 &&
+		 (rp2040_usbpll.cs & PLL_REFDIV) == APP_USB_REFDIV &&
+		 (rp2040_usbpll.fbdiv_int & PLL_FBDIV) == APP_USB_FBDIV &&
+		 (rp2040_usbpll.prim & (PLL_POSTDIV1 | PLL_POSTDIV2)) == APP_USB_POSTDIVS )
 		return;
 
 	/* Reset and re-enable the PLL.
 	*/
-	rp2040_reset(RP2040_RESETS_pll_usb);
+	rp2040_reset(RESETS_pll_usb);
 
 	/* Initialise the USB PLL
 	*/
@@ -98,8 +98,8 @@ void rp2040_usbpll_init(void)
 
 	/* Switch the USB clock to the PLL
 	*/
-	rp2040_clocks.usb.div = RP2040_CLK_DIVBY1;
-	rp2040_clocks.usb.ctrl = RP2040_CLK_ENABLE | RP2040_CLKSRC_USB_PLL_USB;
+	rp2040_clocks.usb.div = CLK_DIVBY1;
+	rp2040_clocks.usb.ctrl = CLK_ENABLE | CLKSRC_USB_PLL_USB;
 }
 
 static void pll_helper(rp2040_pll_t *pll, rp2040_pll_t *pll_w1c, u32_t fbdiv, u32_t postdivs)
@@ -111,11 +111,11 @@ static void pll_helper(rp2040_pll_t *pll, rp2040_pll_t *pll_w1c, u32_t fbdiv, u3
 
 	/* Power on the PLL
 	*/
-	pll_w1c->pwr = RP2040_PLL_VCOPD | RP2040_PLL_PD;
+	pll_w1c->pwr = PLL_VCOPD | PLL_PD;
 
 	/* Wait for the PLL to lock
 	*/
-	do {	/* Wait */	} while ( (pll->cs & RP2040_PLL_LOCK) == 0 );
+	do {	/* Wait */	} while ( (pll->cs & PLL_LOCK) == 0 );
 
 	/* Configure the post-dividers
 	*/
@@ -123,5 +123,5 @@ static void pll_helper(rp2040_pll_t *pll, rp2040_pll_t *pll_w1c, u32_t fbdiv, u3
 
 	/* Power on the post-dividers
 	*/
-	pll_w1c->pwr = RP2040_PLL_POSTDIVPD;
+	pll_w1c->pwr = PLL_POSTDIVPD;
 }
